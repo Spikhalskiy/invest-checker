ebe73cffc9c8d1fdda91334cc224b13613b6d846
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2013-04-03 00:06:04 +0400

    Add summary percents

--- server_logic.py
+++ server_logic.py
@@ -15,35 +15,54 @@ def accounts_summary(period):
                     map(lambda tmstmp: datetime(tmstmp.year, tmstmp.month, tmstmp.day),
                         map(lambda rec: parse(rec.timestamp), records)))))
                 )
-    del dates[0]    # first date and corresponding records used only as helpers in calculation
 
     providers = list(set(map(lambda x: x.provider, records)))
 
+    # form a list of sorted by date lists of records
     data_set = map(lambda name: sorted(filter(lambda rec: rec.account == name, records),
                                        cmp=lambda x, y: cmp(parse(x.timestamp), parse(y.timestamp))),
                    account_names)
 
+    #prepare summary arrays
+    summary_profit_for_date = {}
+    summary_prev_balance_for_date = {}
+    for date in dates:
+        summary_profit_for_date[date] = 0
+        summary_prev_balance_for_date[date] = 0
+
     #process records, add some info for rendering
-    for raw in data_set:
+    for row in data_set:
         prev = None
-        for rec in raw:
+        for rec in row:
             #add date for easy process on client
             timestamp = parse(rec.timestamp)
             rec.date = datetime(timestamp.year, timestamp.month, timestamp.day).strftime("%Y-%m-%d")
 
             #add profit in perc in relation to previous date
             if prev is not None:
-                rec.profit_in_perc = ((rec.balance - prev.balance) - (rec.deposit - prev.deposit)) / rec.balance
+                profit = (rec.balance - prev.balance) - (rec.deposit - prev.deposit)
+                prev_balance = prev.balance
+                rec.profit_in_perc = profit / prev_balance
+                #use later for summary series
+                summary_profit_for_date[rec.date] += profit
+                summary_prev_balance_for_date[rec.date] += prev_balance
             prev = rec
 
-        del raw[0]  # we use first element only for calculating, they are helpers and should be deleted
+    # remove helpers - elements for first additional date
+    del dates[0]    # first date and corresponding records used only as helpers in calculation
+    for row in data_set:
+        del row[0]  # we use first element only for calculating, they are helpers and should be deleted
+    data_set = filter(lambda row: row, data_set) # remove empty rows (this rows contained only one helper element)
 
-    data_set = filter(lambda raw: raw, data_set) # remove empty raws (this raws contained only one helper element)
+    #process summary
+    sumary_perc_profit = map(lambda date: summary_profit_for_date[date] / summary_prev_balance_for_date[date], dates)
 
+    # serialize in json
     result = {
         "dates": dates,
         "providers": providers,
-        "dataset": map(lambda x: map(props, x), data_set)
+        "dataset": map(lambda x: map(props, x), data_set),
+        "summary_profit": sumary_perc_profit
     }
 
     return json.dumps(result)
